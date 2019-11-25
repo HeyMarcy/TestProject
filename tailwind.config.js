85e3a2fc2f4d253d739d9e9d6ed0306b95294372
Marcy Montross <marcy.montross@gmail.com>
2019-11-24 19:17:11 -0600

    adds grid

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  prefix: '',
+  important: false,
+  separator: ':',
+
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          'med-dark': '#767676',
+          'cool-dark': '#6D7278',
+        },
+        blue: {
+          'light': '#67C7EC',
+          'med': '#066D9F',
+          'brand': '#086EA0',
+        },
+        'brand-yellow': '#EEDD23',
+        'brand-red': '#F33437',
+      },
+    },
+    fontFamily: {
+      display: ['hk_grotesk_promedium', 'sans-serif'],
+      body: ['hk_grotesk_prolight', 'sans-serif'],
+    },
+  },
+  variants: {},
+  corePlugins: {},
+  plugins: [],
+}

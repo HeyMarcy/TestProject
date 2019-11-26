a9eb1fef23b9dd1ced7c1c27805abb23d66cadad
Marcy Montross <marcy.montross@gmail.com>
2019-11-26 14:47:39 -0600

    adds section 7 and triangle.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,32 +1,8 @@
-console.log('index.js')
+import { gsap } from 'gsap'
 
+let triTl = gsap.timeline();
 
-	var navMenuDiv = document.getElementById('nav-content');
-	var navMenu = document.getElementById('nav-toggle');
-	
-	document.onclick = check;
-	function check(e){
-	  var target = (e && e.target) || (event && event.srcElement);
-	  
-	  //Nav Menu
-	  if (!checkParent(target, navMenuDiv)) {
-		// click NOT on the menu
-		if (checkParent(target, navMenu)) {
-		  // click on the link
-		  if (navMenuDiv.classList.contains('hidden')) {
-			navMenuDiv.classList.remove('hidden');
-		  } else {navMenuDiv.classList.add('hidden');}
-		} else {
-		  // click both outside link and outside menu, hide menu
-		  navMenuDiv.classList.add('hidden');
-		}
-	  }
-	  
-	}
-	function checkParent(t, elm) {
-	  while(t.parentNode) {
-		if( t == elm ) {return true;}
-		t = t.parentNode;
-	  }
-	  return false;
-	}
+triTl.to('.triangle', 2, { y: 100, backgroundColor: '#4f9d88' }, 0.05)
+  .to('#blue-top', { duration: 2, top: '+=250px', rotation: 180, ease: 'expo.inOut'})
+
+// const blueTriangle = document.getElementById('blue-top')

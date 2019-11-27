bdfda1e0433712163aef58e9b47b97f40853ee88
Marcy Montross <marcy.montross@gmail.com>
2019-11-27 12:20:29 -0600

    adds section 7 animation

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,21 @@
 import { gsap } from 'gsap'
 
+const triBlue = document.getElementById('blueTop')
+const triBlack = document.getElementById('blackMiddle')
+const triRed = document.getElementById('redCenter')
+
+const bluePos = triBlue.getBBox();
+const blackPos = triBlack.getBBox();
+const redPos = triRed.getBBox();
+
+
+console.log('bluePos', bluePos)
+console.log('blackPos', blackPos)
+console.log('redPos', redPos)
+
 let triTl = gsap.timeline();
 
-triTl.to('.triangle', 2, { y: 100, backgroundColor: '#4f9d88' }, 0.05)
-  .to('#blue-top', { duration: 2, top: '+=250px', rotation: 180, ease: 'expo.inOut'})
-
-// const blueTriangle = document.getElementById('blue-top')
+triTl
+  .from(triBlack, { opacity: 0, scale: 5, transformOrigin: '50% 50%', ease: 'back.out', duration: 2})
+  .from(triRed, { opacity: 0, scale: 0, transformOrigin: '50% 50%', stagger: 0.5, ease: 'elastic.out(0.9, 0.3)', duration: 1 }, '+=.05')
+  .from(triBlue, { opacity: 0, y: '-=200', stagger: '0.5', ease: 'bounce.out(0.5, 0.3)', duration: 1 }, '-=.5')
